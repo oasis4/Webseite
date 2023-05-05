01cbc3de435184e4a9a01c10a4df9bf3dc450e73
oasis4 <67016368+oasis4@users.noreply.github.com>
2023-05-05 15:47:56 +0200

    login

---
+++ cgi-bin/htmlgruss3mitexternercss.py
@@ -0,0 +1,85 @@
+#!C:/Python34/python.exe
+
+
+import cgi
+class Formular_einlesen:
+     def __init__ (self):
+          self.geschlecht=""
+          self.passwort=""
+          self.name=""
+          self.mail=""
+          
+     def fehler(self):
+          print ('Content-Type: text/html')
+          print()
+          print ('<?xml version="1.0" ?>\
+             <!DOCTYPE html>\
+            <link rel="stylesheet" type="text/css" href="../cssclasse.css"/>\
+            <head>\
+            <meta http-equiv="Content-type" content="text/html; CHARSET=iso-8859-1"/>\
+             <title> Fehlermeldung </title>\
+              </head>\
+              <body>\
+              <h1>Fehler!</h1>\
+              <p class="white">Bitte die Felder Geschlecht, Vorname, Nachname und E-Mail ausfüllen!</p>\
+              <p>\
+              <input type="submit" value="Zurück" onclick = "history.back()" />\
+              </p>\
+              </body>\
+              </html>')
+     
+     def fehler1(self):
+             print ("Content-Type: text/html")
+             print()
+             print ('<?xml version="1.0" ?>\
+               <!DOCTYPE html>\
+               <link rel="stylesheet" type="text/css" href="../cssclasse.css"/>\
+               <head>\
+                <title> Fehlermeldung </title>\
+                </head>\
+                <body>\
+                <h1>Fehler!</h1>\
+                <p class="white">Die E-Mail-Adressen stimmen nicht überein!</p>\
+                <p>\
+                <input type="submit" value="Zur&uuml;ck" onclick = "history.back()" />\
+                </p>\
+                </body>\
+                </html>')
+
+     def haupt(self):
+          form = cgi.FieldStorage()
+          if "Geschlecht" in form:
+               self.geschlecht = form["Geschlecht"].value
+               if self.geschlecht == "weiblich":
+                    self.anrede = "Frau"
+               if self.geschlecht == "maennlich":
+                    self.anrede = "Herr"
+          if "Vorname" in form:
+             self.vorname = form["Vorname"].value
+          if "Name" in form:
+             self.name=form["Name"].value
+          if "Mail" in form:
+             self.mail=form["Mail"].value
+          if "Mailw" in form:
+             self.mailw=form["Mailw"].value
+          if  (self.geschlecht=="" or self.vorname=="" or self.name=="" or self.mail=="" or self.mailw==""):
+               self.fehler()
+          elif self.mail != self.mailw:
+               self.fehler1()
+          else:
+              print ('Content-Type: text/html')
+              print()
+              print( '<!DOCTYPE html>\
+                 <link rel="stylesheet" type="text/css" href="../cssclasse.css"/>\
+                 <head>\
+                 <meta http-equiv="Content-type" content="text/html; CHARSET=iso-8859-1"/>\
+                 <title> Anmeldung erfolgreich</title>\
+                 </head>\
+                 <body>\
+                 <h1>Vielen Dank für Ihre Daten!</h1>\
+                 <div class="white">Herzlich willkommen, '+self.anrede+' '+self.vorname+' '+self.name+'! Ihre E-Mail-Adresse ist: '+self.mail+' </div>\
+                  </body>\
+                  </html>')
+
+objekt=Formular_einlesen()
+objekt.haupt()
